e48ac4ce3c23ba745b2b92ca92003b42e8c59a64
Htoo Wai Yan <htoowaiyan.dev@gmail.com>
2024-02-01 23:12:03 +0630

    proj initialized
    Spring Boot Intialized. Config MongoDB

---
+++ src/main/java/com/example/demo/Address.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+public class Address {
+    private String country;
+    private String postCode;
+    private String city;
+}
---
+++ src/main/java/com/example/demo/Student.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Gender gender;
+}
